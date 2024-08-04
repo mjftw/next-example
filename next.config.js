f9dfb8dc5988d91915dfe37a66937ac0f10b7774
Merlin Webster <mjftwebster@gmail.com>
2024-08-04 22:48:11 +0100

    Remove the old env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/lib/utils/env.js");
+
 
 /** @type {import("next").NextConfig} */
 const config = {
