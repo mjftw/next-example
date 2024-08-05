c3ddbb3a33c58ae64995d1c500992449f158d0ba
Merlin Webster <mjftwebster@gmail.com>
2024-08-05 08:27:23 +0100

    Configuration is now more generic

--- src/lib/services/configuration.test.ts
+++ src/lib/services/configuration.test.ts
@@ -3,29 +3,21 @@ import { createConfigurationService, type ConfigurationService } from './configu
 import { type EnvRepository } from '../repositories/env';
 
 describe('ConfigurationService', () => {
-  const mockEnvRepository: EnvRepository = {
-    NODE_ENV: 'test',
-    DATABASE_URL: 'mock-db-url',
-    RABBITMQ_HOST: 'localhost',
-    RABBITMQ_PORT: 5672,
-    RABBITMQ_USERNAME: 'guest',
-    RABBITMQ_PASSWORD: 'guest',
+  const mockEnv = {
+    FOO: 'foo',
+    BAR: 'bar',
+    BAZ: 'baz',
   };
 
-  let configService: ConfigurationService;
-
-  beforeEach(() => {
-    configService = createConfigurationService(mockEnvRepository);
-  });
+  const configService = createConfigurationService(mockEnv);
 
   it('should return the correct environment variable', () => {
-    expect(configService.getEnv('NODE_ENV')).toBe('test');
-    expect(configService.getEnv('DATABASE_URL')).toBe('mock-db-url');
-    expect(configService.getEnv('RABBITMQ_HOST')).toBe('localhost');
-    expect(configService.getEnv('RABBITMQ_PORT')).toBe(5672);
+    expect(configService.getEnv('FOO')).toBe('foo');
+    expect(configService.getEnv('BAR')).toBe('bar');
+    expect(configService.getEnv('BAZ')).toBe('baz');
   });
 
   it('should throw an error for non-existent environment variables', () => {
-    expect(() => configService.getEnv('NON_EXISTENT_VAR' as keyof typeof mockEnvRepository)).toThrow();
+    expect(() => configService.getEnv('NON_EXISTENT_VAR' as keyof typeof mockEnv)).toThrow();
   });
 });
--- src/lib/services/configuration.ts
+++ src/lib/services/configuration.ts
@@ -1,16 +1,16 @@
 import { createEnvRepository, EnvRepository } from "../repositories/env";
 
-export interface ConfigurationService<T = EnvRepository> {
+export interface ConfigurationService<T extends Record<string, unknown> = EnvRepository> {
   getEnv: <K extends keyof T>(key: K) => T[K];
 }
 
-export const createConfigurationService = (envRepository: EnvRepository): ConfigurationService<typeof envRepository> => {
+export const createConfigurationService = <T extends Record<string, unknown>>(envRepository: T): ConfigurationService<T> => {
 
   return {
-    getEnv: <K extends keyof typeof envRepository>(key: K): (typeof envRepository)[K] => {
+    getEnv: <K extends keyof T>(key: K): T[K] => {
       const value = envRepository[key];
       if (!value) {
-        throw new Error(`Environment variable ${key} not found`);
+        throw new Error(`Environment variable ${String(key)} not found`);
       }
       return value;
     },
